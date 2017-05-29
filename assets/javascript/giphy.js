e77cef372437bdf15754d2badcda22d01c07d111
jcostopulos <costopulos@gmail.com>
2017-05-29 15:29:07 -0700

    buttons calling name-data

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,15 +1,15 @@
 //PSEUDOCODE
-//When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+//When the user clicks on a button, the page should grab 10 TODO:static, non-animated gif images from the GIPHY API and place them on the page.
 
-// When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+//TODO When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
-// Under every gif, display its rating (PG, G, so on).
-
-// This data is provided by the GIPHY API.
+//TODO Under every gif, display its rating (PG, G, so on).
 
 // Only once you get images displaying with button presses should you move on to the next step.
 
-// Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+// Add a form to your page that takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array AND remakes the buttons on the page.
+    //TODO use .val() to get value of form input.
+
 
 // Rejoice! You just made something really cool.
 
@@ -76,9 +76,8 @@ $(document).on("click", ".topicButton", callGifs);
 
 renderButtons();
 
-});
+});//document ready endtag
 
-//document ready endtag
 // $(".gifSrc").click( //animated or static gif
 //        function()
 //        {
